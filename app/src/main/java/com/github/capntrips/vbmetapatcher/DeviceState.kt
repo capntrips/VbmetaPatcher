9bfc24f0b8e8065e6adb8b090eda5e97c52ceaec
capntrips <31455058+capntrips@users.noreply.github.com>
2022-05-21 21:54:11 -0500

    added error handling for failed root commands

--- app/src/main/java/com/github/capntrips/vbmetapatcher/DeviceState.kt
+++ app/src/main/java/com/github/capntrips/vbmetapatcher/DeviceState.kt
@@ -1,13 +1,23 @@
 package com.github.capntrips.vbmetapatcher
 
 import android.content.Context
+import android.util.Log
+import android.widget.Toast
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.topjohnwu.superuser.Shell
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 import java.io.File
 
-class DeviceState constructor(context: Context, _isRefreshing : MutableStateFlow<Boolean>) : DeviceStateInterface {
+class DeviceState constructor(context: Context, _isRefreshing : MutableStateFlow<Boolean>) : ViewModel(), DeviceStateInterface {
+    companion object {
+        const val TAG: String = "VbmetaPatcher/DeviceState"
+    }
+
     private var _slotA: MutableStateFlow<SlotStateInterface>
     private var _slotB: MutableStateFlow<SlotStateInterface>
     override val slotSuffix: String
@@ -26,15 +36,37 @@ class DeviceState constructor(context: Context, _isRefreshing : MutableStateFlow
         // https://android.googlesource.com/platform/system/update_engine/+/refs/tags/android-12.0.0_r12/aosp/dynamic_partition_control_android.cc#393
         // https://android.googlesource.com/platform/system/core/+/refs/tags/android-12.0.0_r12/fs_mgr/fs_mgr_fstab.cpp#416
         // https://android.googlesource.com/platform/system/core/+/refs/tags/android-12.0.0_r12/fs_mgr/fs_mgr_boot_config.cpp#156
-        val hardwarePlatform = Shell.su("getprop ro.boot.hardware.platform").exec().out[0]
-        val misc = File(Shell.su("cat /vendor/etc/fstab.$hardwarePlatform | grep /misc | awk '{ print \$1 }'").exec().out[0])
+        val hardwarePlatformResult = Shell.su("getprop ro.boot.hardware.platform").exec()
+        if (hardwarePlatformResult.out.isEmpty()) {
+            log(context, "Failed to get ro.boot.hardware.platform", shouldThrow = true)
+        }
+        val hardwarePlatform = hardwarePlatformResult.out[0]
+        val miscResult = Shell.su("cat /vendor/etc/fstab.$hardwarePlatform | grep /misc | awk '{ print \$1 }'").exec()
+        if (miscResult.out.isEmpty()) {
+            log(context, "Failed to find /misc", shouldThrow = true)
+        }
+        val misc = File(miscResult.out[0])
         val vbmetaA = misc.resolveSibling("vbmeta_a")
         val vbmetaB = misc.resolveSibling("vbmeta_b")
 
         // https://android.googlesource.com/device/google/gs101/+/refs/tags/android-12.0.0_r12/interfaces/boot/1.2/BootControl.cpp#194
-        slotSuffix = Shell.su("getprop ro.boot.slot_suffix").exec().out[0]
+        val slotSuffixResult = Shell.su("getprop ro.boot.slot_suffix").exec()
+        if (slotSuffixResult.out.isEmpty()) {
+            log(context, "Failed to get ro.boot.slot_suffix", shouldThrow = true)
+        }
+        slotSuffix = slotSuffixResult.out[0]
 
         _slotA = MutableStateFlow(SlotState(context, vbmetaA, _isRefreshing))
         _slotB = MutableStateFlow(SlotState(context, vbmetaB, _isRefreshing))
+    }
+
+    private fun log(context: Context, message: String, shouldThrow: Boolean = false) {
+        viewModelScope.launch(Dispatchers.Main) {
+            Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+        }
+        Log.d(SlotState.TAG, message)
+        if (shouldThrow) {
+            throw Exception(message)
+        }
     }
 }
--- app/src/main/java/com/github/capntrips/vbmetapatcher/SlotState.kt
+++ app/src/main/java/com/github/capntrips/vbmetapatcher/SlotState.kt
@@ -37,10 +37,14 @@ class SlotState(context: Context, private val vbmeta: File, private val _isRefre
     }
 
     override fun refresh(context: Context) {
-        if (!hasMagic() || !hasAvbRelease()) {
+        if (!hasMagic(context) || !hasAvbRelease(context)) {
             log(context, "Unexpected Format", shouldThrow = true)
         }
-        val data = Base64.decode(Shell.su("dd if=$vbmeta bs=1 count=$AVB_VBMETA_IMAGE_HEADER_SIZE status=none | base64 -w 0").exec().out[0], Base64.DEFAULT)
+        val dataResult = Shell.su("dd if=$vbmeta bs=1 count=$AVB_VBMETA_IMAGE_HEADER_SIZE status=none | base64 -w 0").exec()
+        if (dataResult.out.isEmpty()) {
+            log(context, "Failed to get vbmeta header", shouldThrow = true)
+        }
+        val data = Base64.decode(dataResult.out[0], Base64.DEFAULT)
         val buffer = ByteBuffer.wrap(data)
 
         // https://android.googlesource.com/platform/external/avb/+/refs/tags/android-12.0.0_r12/libavb/avb_vbmeta_image.h#134
@@ -63,7 +67,11 @@ class SlotState(context: Context, private val vbmeta: File, private val _isRefre
             if (isImage) {
                 Shell.su("printf '\\x00' | dd of=$vbmeta bs=1 seek=123 count=1 conv=notrunc status=none").exec()
                 patchStatus = PatchStatus.Stock
-                sha1 = Shell.su("dd if=$vbmeta bs=1 count=$imageSize status=none | sha1sum | awk '{ print \$1 }'").exec().out[0]
+                val sha1Result = Shell.su("dd if=$vbmeta bs=1 count=$imageSize status=none | sha1sum | awk '{ print \$1 }'").exec()
+                if (sha1Result.out.isEmpty()) {
+                    log(context, "Failed to get boot SHA1", shouldThrow = true)
+                }
+                sha1 = sha1Result.out[0]
             } else {
                 Shell.su("dd if=$vbmeta of=vbmeta.img bs=1 count=$imageSize status=none").exec()
                 val file = File(context.filesDir, "vbmeta.img")
@@ -72,13 +80,21 @@ class SlotState(context: Context, private val vbmeta: File, private val _isRefre
                 file.delete()
             }
         } else {
-            sha1 = Shell.su("dd if=$vbmeta bs=1 count=$imageSize status=none | sha1sum | awk '{ print \$1 }'").exec().out[0]
+            val sha1Result = Shell.su("dd if=$vbmeta bs=1 count=$imageSize status=none | sha1sum | awk '{ print \$1 }'").exec()
+            if (sha1Result.out.isEmpty()) {
+                log(context, "Failed to get boot SHA1", shouldThrow = true)
+            }
+            sha1 = sha1Result.out[0]
         }
     }
 
-    private fun refreshPatchStatus() {
+    private fun refreshPatchStatus(context: Context) {
         // https://android.googlesource.com/platform/external/avb/+/refs/tags/android-12.0.0_r12/libavb/avb_vbmeta_image.h#174
-        patchStatus = if (Shell.su("dd if=$vbmeta bs=1 skip=123 count=1 status=none | xxd -p").exec().out[0] == "03") {
+        val patchStatusResult = Shell.su("dd if=$vbmeta bs=1 skip=123 count=1 status=none | xxd -p").exec()
+        if (patchStatusResult.out.isEmpty()) {
+            log(context, "Failed to get patch status", shouldThrow = true)
+        }
+        patchStatus = if (patchStatusResult.out[0] == "03") {
             PatchStatus.Patched
         } else {
             PatchStatus.Stock
@@ -120,14 +136,14 @@ class SlotState(context: Context, private val vbmeta: File, private val _isRefre
     }
 
     private fun setFlags(context: Context, toStatus: PatchStatus): Boolean {
-        if (!hasMagic() || !hasAvbRelease()) {
+        if (!hasMagic(context) || !hasAvbRelease(context)) {
             log(context, "Unexpected Format", shouldThrow = true)
         }
         return if (Shell.su("blockdev --setrw $vbmeta").exec().isSuccess) {
             val byte = if (toStatus == PatchStatus.Patched) "03" else "00"
             Shell.su("printf '\\x$byte' | dd of=$vbmeta bs=1 seek=123 count=1 conv=notrunc status=none").exec()
             Shell.su("blockdev --setro $vbmeta").exec()
-            refreshPatchStatus()
+            refreshPatchStatus(context)
             true
         } else {
             log(context, "Failed to setrw ${vbmeta.name}")
@@ -135,14 +151,22 @@ class SlotState(context: Context, private val vbmeta: File, private val _isRefre
         }
     }
 
-    private fun hasMagic() : Boolean {
+    private fun hasMagic(context: Context) : Boolean {
         // https://android.googlesource.com/platform/external/avb/+/refs/tags/android-12.0.0_r12/libavb/avb_vbmeta_image.h#126
-        return Shell.su("dd if=$vbmeta bs=1 count=4 status=none").exec().out[0] == MAGIC
+        val magicResult = Shell.su("dd if=$vbmeta bs=1 count=4 status=none").exec()
+        if (magicResult.out.isEmpty()) {
+            log(context, "Failed to get magic", shouldThrow = true)
+        }
+        return magicResult.out[0] == MAGIC
     }
 
-    private fun hasAvbRelease() : Boolean {
+    private fun hasAvbRelease(context: Context) : Boolean {
         // https://android.googlesource.com/platform/external/avb/+/refs/tags/android-12.0.0_r12/libavb/avb_vbmeta_image.h#186
-        return Shell.su("dd if=$vbmeta bs=1 skip=128 count=48 status=none").exec().out[0].startsWith(AVBTOOL)
+        val avbReleaseResult = Shell.su("dd if=$vbmeta bs=1 skip=128 count=48 status=none").exec()
+        if (avbReleaseResult.out.isEmpty()) {
+            log(context, "Failed to get avbtool release", shouldThrow = true)
+        }
+        return avbReleaseResult.out[0].startsWith(AVBTOOL)
     }
 
     // https://android.googlesource.com/platform/external/avb/+/refs/tags/android-12.0.0_r12/avbtool.py#203
